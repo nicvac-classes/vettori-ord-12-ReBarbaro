24d87897588be15dadc21a53855da75efad03791
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2025-03-19 16:59:34 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include<vector>
 using namespace std;
-
+    
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    srand(time(0)); 
+    
+    int n, i, j, t;
+
+    cout << "quante cifre bisogna inserire?" << endl;
+    cin >> n;
+    int r[n];
+    
+
+    i = 0;
+    while (i < n) {
+        r[i] = rand() % (n * 10 + 1);
+        i = i + 1;
+    }
+    i = 0;
+    t = 0;
+    j = 0;
+    while (i < n) {
+        j = 0;
+        while (j < n - 1) {
+            if (r[j] > r[j + 1]) {
+                t = r[j];
+                r[j] = r[j + 1];
+                r[j + 1] = t;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        cout << r[i] << endl;
+        i = i + 1;
+    }
+    
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
